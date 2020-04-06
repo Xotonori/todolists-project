a22b51298ce39b36d91d4f7affedf3379cedf0f6
Xotonori <xotonori29011991@gmail.com>
2020-04-06 23:58:59 +0300

    Lesson 02 is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import TodoListHeader from "./components/TodoListHeader"
+import TodoListTasks from "./components/TodoListTasks"
+import TodoListFooter from "./components/TodoListFooter"
 
 function App() {
+
+    let tasks = [
+        {title:'ReactJs', isDone:false, priority: 'low'},
+        {title:'CSS', isDone:false, priority: 'low'},
+        {title:'JS', isDone:false, priority: 'high'},
+        {title:'jQuery', isDone:true, priority: 'medium'},
+        {title:'Patterns', isDone:true, priority: 'low'},
+    ];
+
+    let filterValue = "Completed";
+
   return (
-    <div className="App">
-      test
-    </div>
+      <div className="App">
+          <div className="todoList">
+             <TodoListHeader />
+             <TodoListTasks tasks={tasks}/>
+             <TodoListFooter filterValue={filterValue}/>
+          </div>
+      </div>
   );
 }
 

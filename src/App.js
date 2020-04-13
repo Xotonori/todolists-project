17bfe837f4fa6dd2576c8ff12bf293364ceae996
Xotonori <xotonori29011991@gmail.com>
2020-04-14 00:04:10 +0300

    Lesson 03 is done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import TodoListHeader from "./components/TodoListHeader"
 import TodoListTasks from "./components/TodoListTasks"
 import TodoListFooter from "./components/TodoListFooter"
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-    let tasks = [
-        {title:'ReactJs', isDone:false, priority: 'low'},
-        {title:'CSS', isDone:false, priority: 'low'},
-        {title:'JS', isDone:false, priority: 'high'},
-        {title:'jQuery', isDone:true, priority: 'medium'},
-        {title:'Patterns', isDone:true, priority: 'low'},
-    ];
 
-    let filterValue = "Completed";
+    state = {
+        tasks: [
+            {title: 'ReactJs', isDone: false, priority: 'low'},
+            {title: 'CSS', isDone: false, priority: 'low'},
+            {title: 'JS', isDone: false, priority: 'high'},
+            {title: 'jQuery', isDone: true, priority: 'medium'},
+            {title: 'Patterns', isDone: true, priority: 'low'},
+        ],
 
-  return (
-      <div className="App">
-          <div className="todoList">
-             <TodoListHeader />
-             <TodoListTasks tasks={tasks}/>
-             <TodoListFooter filterValue={filterValue}/>
-          </div>
-      </div>
-  );
+        filterValue: "Completed",
+    };
+
+    addTask = (newTitle) => {
+        this.setState({
+            tasks: [...this.state.tasks, {title: newTitle, isDone: false, priority: 'low'}]
+        })
+    };
+
+
+    render() {
+        return (
+            <div className="App">
+                <div className="todoList">
+                    <TodoListHeader addTask={this.addTask}/>
+                    <TodoListTasks tasks={this.state.tasks}/>
+                    <TodoListFooter filterValue={this.state.filterValue}/>
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default App;

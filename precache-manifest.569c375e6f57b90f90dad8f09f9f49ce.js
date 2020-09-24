5ee42db15051a467412ac13e4060ce6fb169d786
Xotonori <xotonori29011991@gmail.com>
2020-09-24 23:26:41 +0300

    Updates

--- precache-manifest.569c375e6f57b90f90dad8f09f9f49ce.js
+++ precache-manifest.569c375e6f57b90f90dad8f09f9f49ce.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2b59f453c34684bff8eaaac3cd7fffc0",
+    "revision": "728c76db6be05cc551129941542fdfab",
     "url": "/todolists-project/index.html"
   },
   {
-    "revision": "91d75be0bd9dea0c075a",
+    "revision": "514fc44e6975097b6ff9",
     "url": "/todolists-project/static/css/main.222020f2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/todolists-project/static/js/2.0fe1b6c3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "91d75be0bd9dea0c075a",
-    "url": "/todolists-project/static/js/main.f03622a7.chunk.js"
+    "revision": "514fc44e6975097b6ff9",
+    "url": "/todolists-project/static/js/main.f11a8bcc.chunk.js"
   },
   {
     "revision": "662f1f463d71befba9c1",
